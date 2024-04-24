f9894d18cc752c8d220cf94b480f3252a386fc45
Sahena <sahenamaharjan@gmail.com>
2024-04-24 12:24:40 +0100

    Initial Commit

---
+++ src/main/java/com/bizzjen/shopido/ShopidoApplication.java
@@ -0,0 +1,13 @@
+package com.bizzjen.shopido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizzjen/shopido/ShopidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizzjen.shopido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
